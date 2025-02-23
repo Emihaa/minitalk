da1ffa0027947d8acdbd11af04cc69c725e57f9c
Emilia Haanpää <emihaanpaa@gmail.com>
2025-02-23 18:47:24 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ehaanpaa <ehaanpaa@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 18:11:32 by ehaanpaa          #+#    #+#             */
-/*   Updated: 2025/01/31 19:29:02 by ehaanpaa         ###   ########.fr       */
+/*   Updated: 2025/02/23 18:37:43 by ehaanpaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,95 +14,52 @@
 
 //take string and convert it to binary
 //send binary to server
-static void send_signal(char *s, int served_pid)
+static void	send_signal(char *s, int served_pid)
 {
-    int i = 0; 
-    while (s[i])
-    {
-        if (s[i] == '1')
-            kill(served_pid, SIGUSR1);
-        else if (s[i] == '0')
-            kill(served_pid, SIGUSR2);
-        i++;
-        usleep(500);
-    }
+	int	bits;
+	int	c;
+	int	i;
+
+	i = 0;
+	while (1)
+	{
+		c = s[i];
+		bits = 0;
+		while (bits < 8)
+		{
+			if (((unsigned char)(c >> (7 - bits)) & 1) == 0)
+				kill(served_pid, SIGUSR1);
+			else if (((unsigned char)(c >> (7 - bits)) & 1) == 1)
+				kill(served_pid, SIGUSR2);
+			usleep(250);
+			bits++;
+		}
+		bits = 0;
+		if (s[i] == '\0')
+			break ;
+		i++;
+	}
 }
 
-static void binary_convert(int value, int server_pid)
+int	main(int argv, char *argc[])
 {
-    int max = 128;
-    int i = 0;
-    char s[8];
-    
-    ft_printf("\nog: %d\n", value);
-    while (i < 8)
-    {
-        if (value >= max)
-        {
-            value -= max;
-            s[i] = '1';
-        }
-        else
-            s[i] = '0';
-        max /= 2;
-        i++;
-    }
-    s[i] = '\0';
-    ft_printf("binary: %s\n", s);
-    send_signal(s, server_pid);
-}
+	int	server_pid;
 
-// static void send_signal2(char c, int served_pid)
-// {
-//     int i = 0; 
-//     while (i < 8)
-//     {
-//         if (s[i] == '1')
-//             kill(served_pid, SIGUSR1);
-//         else if (s[i] == '0')
-//             kill(served_pid, SIGUSR2);
-//         i++;
-//         usleep(500);
-//     }
-// }
-
-static void ascii_convert(char *s, int server_pid)
-{
-    int i = 0;
-    int ascii = 0;
-    char c = 0;
-    
-    while (s[i] != '\0')
-    {
-        ascii = s[i];
-        c = s[i];
-        // send_signal2(c, server_pid);
-        binary_convert(ascii, server_pid);
-        i++;
-    }
-    binary_convert(0, server_pid);
-    ft_printf("\n");
-}
-
-int main (int argv, char *argc[])
-{
-    int server_pid = 0;
-    
-    if (argv == 3)
-    {
-        server_pid = ft_atoi(argc[1]);
-        if (!server_pid)
-        {
-            ft_printf("Wrong <PID>");
-            return (0);
-        }
-        ascii_convert(argc[2], server_pid);
-
-    }
-    else
-    {
-        ft_printf("Incorrect argument amount.\n");
-        ft_printf("Enter arguments: ./client <PID> <STRING>.\n");
-    }
-    return (0);
+	server_pid = 0;
+	if (argv == 3)
+	{
+		server_pid = ft_atoi(argc[1]);
+		if (!server_pid)
+		{
+			ft_printf("Wrong <PID>");
+			return (0);
+		}
+		send_signal(argc[2], server_pid);
+	}
+	else
+	{
+		ft_printf("Incorrect argument amount.\n");
+		ft_printf("Enter arguments: ./client <PID> <STRING>.\n");
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,54 +6,51 @@
 /*   By: ehaanpaa <ehaanpaa@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:38:06 by ehaanpaa          #+#    #+#             */
-/*   Updated: 2025/01/31 19:35:47 by ehaanpaa         ###   ########.fr       */
+/*   Updated: 2025/02/23 18:42:08 by ehaanpaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int signal)
+void	signal_handler(int signal)
 {
-    static char c = 0;
-    static int i = 7;
-    static char buffer[100000];
-    static int j = 0;
-    
-    if (signal == SIGUSR1)
-    {
-        c += (1 << i);
-    }
-    i--;
-    if (i < 0)
-    {        
-        buffer[j] = c;
-        i = 7;
-        j++;
-        if (c == '\0')
-        {
-            buffer[j] = '\0';
-            ft_printf("%s\n", buffer);
-            j = 0;
-        }
-        c = 0;
-    }
+	static char	c = 0;
+	static int	i = 7;
+	static char	buffer[200000];
+	static int	j = 0;
+
+	if (signal == SIGUSR2)
+		c += (1 << i);
+	i--;
+	if (i < 0)
+	{
+		buffer[j] = c;
+		i = 7;
+		j++;
+		if (c == '\0')
+		{
+			buffer[j] = '\0';
+			ft_printf("%s\n", buffer);
+			j = 0;
+		}
+		c = 0;
+	}
 }
 
-int main (void)
+int	main(void)
 {
-    struct sigaction sa;
-    
-    sa.sa_handler = &signal_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
+	struct sigaction	sa;
 
-    ft_printf("Server pid: %d\n", getpid());
-
-    while (1)
-        pause();
-        
-    return (0);
+	sa.sa_handler = &signal_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server pid: %d\n", getpid());
+	while (1)
+	{
+	}
+	return (0);
 }
